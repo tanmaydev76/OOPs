9c5de5114a3697db0ff00541bd9d4220ae60ea70
tanmaydev76 <tanmaykamble3107@gmail.com>
2026-02-25 01:18:27 +0530

    method overloading

---
+++ polymorphism/Calculator.java
@@ -0,0 +1,20 @@
+package polymorphism;
+
+public class Calculator {
+    // Method Overloading  
+    int add(int a, int b){
+        return a + b;
+    }
+
+    int add(int a, int b, int c){
+        return a + b + c;
+    }
+
+    double add(int a, int b,double d){
+        return a + b + d; 
+    }
+
+    // Operator overloading 
+    // Java does not support user-defined operator overloading but internally overloads certain operators (e.g., + for) String concatenation and integer addition).
+
+}
---
+++ polymorphism/Main.java
@@ -0,0 +1,12 @@
+package polymorphism;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        
+        System.out.println(c.add(5, 04));
+        System.out.println(c.add(2,30,40));
+        System.out.println(c.add(2, 3, 1.5));
+    }
+}
